7bb2e5ceccdf825e7afc9fabd7d31fb4a6c67f5f
arash <arashsafdarpour@gmail.com>
2025-08-03 15:25:10 +0330

    ex 2

--- C1W2/LR.py
+++ C1W2/LR.py
@@ -20,3 +20,14 @@ For instance, you can access `m_train` by writing `train_set_x_orig.shape[0]`.
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
+
+
+'''
+**Exercise:** Reshape the training and test data sets so that images of size (num_px, num_px, 3) are flattened into single vectors of shape (num\_px $*$ num\_px $*$ 3, 1).
+A trick when you want to flatten a matrix X of shape (a,b,c,d) to a matrix X_flatten of shape (b$*$c$*$d, a) is to use: 
+
+X_flatten = X.reshape(X.shape[0], -1).T      # X.T is the transpose of X
+'''
+
+train_set_x_flatten = train_set_x_orig.reshape(num_px*num_px*3, m_train)
+test_set_x_flatten = test_set_x_orig.reshape(num_px*num_px*3, m_test)
