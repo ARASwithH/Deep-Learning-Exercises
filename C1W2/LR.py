c24046b08b0e8aa1ace730b708995090b48b5214
arash <arashsafdarpour@gmail.com>
2025-08-03 15:18:48 +0330

    ex 1

--- C1W2/LR.py
+++ C1W2/LR.py
@@ -6,3 +6,17 @@ from PIL import Image
 from lr_utils import load_dataset
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+
+'''
+**Exercise:** Find the values for:
+    - m_train (number of training examples)
+    - m_test (number of test examples)
+    - num_px (= height = width of a training image)
+Remember that `train_set_x_orig` is a numpy-array of shape (m_train, num_px, num_px, 3).
+For instance, you can access `m_train` by writing `train_set_x_orig.shape[0]`.
+'''
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
