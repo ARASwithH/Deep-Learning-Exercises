0bd1caa4ba32b2b4adc6eb3e00c36939ae155c84
arash <arashsafdarpour@gmail.com>
2025-08-04 12:14:38 +0330

    import dataset

--- C1W3/data_classification.py
+++ C1W3/data_classification.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from testCases_v2 import *
+from sklearn.model_selection import train_test_split
 import sklearn
 import sklearn.datasets
+from sklearn.metrics import classification_report
 import sklearn.linear_model
 from planar_utils import (
     plot_decision_boundary,
@@ -12,3 +14,23 @@ from planar_utils import (
 )
 
 np.random.seed(1)  # set a seed so that the results are consistent
+
+
+
+
+# Dataset
+
+X, Y = load_planar_dataset()
+
+'''
+**Exercise**: How many training examples do you have? In addition, what is the `shape` of the variables `X` and `Y`? 
+**Hint**: How do you get the shape of a numpy array? [(help)](https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.shape.html)
+'''
+X_shape = X.shape
+Y_shape = Y.shape
+m = X_shape[1]
+
+
+
+
+
