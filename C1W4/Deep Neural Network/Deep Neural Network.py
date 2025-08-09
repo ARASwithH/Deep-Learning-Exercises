25576d235105699b17eec22930f2474e37545bf3
arash <arashsafdarpour@gmail.com>
2025-08-09 14:39:48 +0330

    init params func implemetion

--- C1W4/Deep Neural Network/Deep Neural Network.py
+++ C1W4/Deep Neural Network/Deep Neural Network.py
@@ -9,3 +9,34 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 np.random.seed(1)
+
+
+
+
+# L-layer Neural Network 
+'''
+**Exercise**: Implement initialization for an L-layer Neural Network. 
+
+**Instructions**:
+- The model's structure is *[LINEAR -> RELU] $ \times$ (L-1) -> LINEAR -> SIGMOID*. I.e., it has $L-1$ layers using a ReLU activation function followed by an output layer with a sigmoid activation function.
+- Use random initialization for the weight matrices. Use `np.random.randn(shape) * 0.01`.
+- Use zeros initialization for the biases. Use `np.zeros(shape)`.
+- We will store $n^{[l]}$, the number of units in different layers, in a variable `layer_dims`. For example, the `layer_dims` for the "Planar Data classification model" from last week would have been [2,4,1]: There were two inputs, one hidden layer with 4 hidden units, and an output layer with 1 output unit. This means `W1`'s shape was (4,2), `b1` was (4,1), `W2` was (1,4) and `b2` was (1,1). Now you will generalize this to $L$ layers! 
+- Here is the implementation for $L=1$ (one layer neural network). It should inspire you to implement the general case (L-layer neural network).
+```python
+    if L == 1:
+        parameters["W" + str(L)] = np.random.randn(layer_dims[1], layer_dims[0]) * 0.01
+        parameters["b" + str(L)] = np.zeros((layer_dims[1], 1))
+```
+'''
+
+def initialization_params(dims):
+    params = {}
+    n = len(dims)
+
+    for i in range(n-1):
+        params["W" + str(i+1)] = np.random.randn(dims[i+1], dims[i]) * 0.01
+        params["b" + str(i+1)] = np.zeros((dims[i+1], 1))
+
+    return params
+
